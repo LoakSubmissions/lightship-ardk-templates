c4a188585543c181cbd89fc46323f48ac2800b82
Sean Taylor <silverwolf4350@gmail.com>
2023-04-26 14:19:25 -0500

    Started leaderboard script.

---
+++ Assets/Loak/Scripts/LoakLeaderboard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoakLeaderboard : MonoBehaviour
+{
+    private enum LeaderboardConfiguration {Friend, Global, Both}
+    [SerializeField] private LeaderboardConfiguration leaderboardConfiguration;
+    
+    public int numberOfEntries = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
