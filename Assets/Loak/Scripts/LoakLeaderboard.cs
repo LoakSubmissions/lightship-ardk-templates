a1e65fa4bfac933d176af25d1d897539b7aa6e65
Sean Taylor <silverwolf4350@gmail.com>
2023-04-26 18:33:54 -0500

    Added tab functionality to leaderboard UI.

--- Assets/Loak/Scripts/LoakLeaderboard.cs
+++ Assets/Loak/Scripts/LoakLeaderboard.cs
@@ -1,23 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LoakLeaderboard : MonoBehaviour
+namespace Loak.Unity
 {
-    private enum LeaderboardConfiguration {Friend, Global, Both}
-    [SerializeField] private LeaderboardConfiguration leaderboardConfiguration;
-    
-    public int numberOfEntries = 10;
-
-    // Start is called before the first frame update
-    void Start()
+    public class LoakLeaderboard : MonoBehaviour
     {
-        
+        public static LoakLeaderboard Instance;
+
+        private enum LeaderboardConfiguration {Friend, Global, Both}
+        [SerializeField] private LeaderboardConfiguration leaderboardConfiguration = LeaderboardConfiguration.Both;
+
+        public int numberOfEntries = 10;
+
+        private LeaderboardUIBehaviour listItemPrefab;
+        private List<LeaderboardUIBehaviour> listItems = new List<LeaderboardUIBehaviour>();
+
+        private GameObject loadingView;
+        private GameObject list;
+
+        private Tab activeTab;
+        private Tab friendsTab;
+        private Tab globalTab;
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            friendsTab = new Tab(transform.GetChild(1).GetChild(2).GetChild(0).gameObject, null);
+            globalTab = new Tab(transform.GetChild(1).GetChild(2).GetChild(1).gameObject, null);
+            loadingView = transform.GetChild(1).GetChild(4).gameObject;
+
+            switch (leaderboardConfiguration)
+            {
+                case LeaderboardConfiguration.Friend:
+                    friendsTab.SetActive(true);
+                    globalTab.SetActive(false);
+                    ActivateTab(friendsTab);
+                    break;
+
+                case LeaderboardConfiguration.Global:
+                    friendsTab.SetActive(false);
+                    globalTab.SetActive(true);
+                    ActivateTab(globalTab);
+                    break;
+
+                default:
+                    friendsTab.SetActive(true);
+                    globalTab.SetActive(true);
+                    ActivateTab(friendsTab);
+                    break;
+            }
+
+            listItemPrefab = GetComponentInChildren<LeaderboardUIBehaviour>(true);
+            list = listItemPrefab.transform.parent.parent.parent.gameObject;
+            listItemPrefab.gameObject.SetActive(false);
+
+            if (listItems.Count == 0)
+                listItems.Add(listItemPrefab);
+
+            var parent = listItemPrefab.transform.parent;
+            for (int i = listItems.Count; i < numberOfEntries; i++)
+            {
+                listItems.Add(Instantiate(listItemPrefab, parent));
+                listItems[i].gameObject.SetActive(false);
+            }
+
+            loadingView.SetActive(true);
+            list.SetActive(false);
+        }
+
+        internal void ActivateTab(Tab tab)
+        {
+            tab.ToggleSelected(true);
+            SetUI(tab.entries);
+
+            if (activeTab != null)
+                activeTab.ToggleSelected(false);
+            activeTab = tab;
+        }
+
+        private void SetUI(List<(string, long)> entries)
+        {
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    internal class Tab {
+        private GameObject uiObject;
+        private Button button;
+        private Image bullet;
+        public List<(string, long)> entries;
+
+        public Tab(GameObject uiObject, List<(string, long)> entries)
+        {
+            this.uiObject = uiObject;
+            this.button = uiObject.GetComponent<Button>();
+            this.bullet = uiObject.GetComponentInChildren<Image>(true);
+            this.entries = entries;
+
+            var tab = this;
+            button.onClick.AddListener(() => {LoakLeaderboard.Instance.ActivateTab(tab);});
+        }
+
+        public void SetActive(bool val)
+        {
+            uiObject.SetActive(val);
+        }
+
+        public void Update(List<(string, long)> entries)
+        {
+            this.entries = entries;
+        }
+
+        public void ToggleSelected(bool val)
+        {
+            bullet.enabled = val;
+            button.interactable = !val;
+        }
     }
 }
--- Assets/Loak/Scripts/LoakScanner.cs
+++ Assets/Loak/Scripts/LoakScanner.cs
@@ -6,98 +6,101 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using Niantic.ARDK.Extensions.Meshing;
 
-public class LoakScanner : MonoBehaviour
+namespace Loak.Unity
 {
-    [Header("Config")]
-    [Tooltip("Number of mesh blocks required before scanning is complete.")]
-    public int scanThreshold = 20;
-    [Tooltip("True if the scan should begin immediately on Start. Set to false if you wish to start it manually.")]
-    [SerializeField] private bool autoStart = true;
-
-    [Header("Events")]
-    [Tooltip("Invoked when the scan initially starts and the UI turns on.")]
-    public UnityEvent OnScanStart = new UnityEvent();
-    [Tooltip("Invoked when the scan finishes and the UI turns off.")]
-    public UnityEvent OnScanEnd = new UnityEvent();
-
-    private GameObject scanCanvas;
-    private TMP_Text scanText;
-    private Image fillBar;
-    private ARMeshManager meshMan;
-    private string scanningString = "Building mesh...";
-    private string completeString = "Mesh complete!";
-    private float scanProgress = 0f;
-    private bool scanning = false;
-
-    // Sets up all private reference variables and begins scan if auto start enabled.
-    void Start()
+    public class LoakScanner : MonoBehaviour
     {
-        scanCanvas = transform.GetComponentInChildren<Canvas>(true).gameObject;
-        scanText = scanCanvas.GetComponentInChildren<TMP_Text>(true);
-        fillBar = scanCanvas.GetComponentsInChildren<Image>(true)[1];
-        meshMan = FindObjectOfType<ARMeshManager>(true);
+        [Header("Config")]
+        [Tooltip("Number of mesh blocks required before scanning is complete.")]
+        public int scanThreshold = 20;
+        [Tooltip("True if the scan should begin immediately on Start. Set to false if you wish to start it manually.")]
+        [SerializeField] private bool autoStart = true;
 
-        if (meshMan == null)
+        [Header("Events")]
+        [Tooltip("Invoked when the scan initially starts and the UI turns on.")]
+        public UnityEvent OnScanStart = new UnityEvent();
+        [Tooltip("Invoked when the scan finishes and the UI turns off.")]
+        public UnityEvent OnScanEnd = new UnityEvent();
+
+        private GameObject scanCanvas;
+        private TMP_Text scanText;
+        private Image fillBar;
+        private ARMeshManager meshMan;
+        private string scanningString = "Building mesh...";
+        private string completeString = "Mesh complete!";
+        private float scanProgress = 0f;
+        private bool scanning = false;
+
+        // Sets up all private reference variables and begins scan if auto start enabled.
+        void Start()
         {
-            Debug.LogError("Loak Scanner requires an ARMeshManager in the scene. Please add one or remove the Loak Scanner.");
-            enabled = false;
-            return;
+            scanCanvas = transform.GetComponentInChildren<Canvas>(true).gameObject;
+            scanText = scanCanvas.GetComponentInChildren<TMP_Text>(true);
+            fillBar = scanCanvas.GetComponentsInChildren<Image>(true)[1];
+            meshMan = FindObjectOfType<ARMeshManager>(true);
+
+            if (meshMan == null)
+            {
+                Debug.LogError("Loak Scanner requires an ARMeshManager in the scene. Please add one or remove the Loak Scanner.");
+                enabled = false;
+                return;
+            }
+
+            meshMan.GenerateUnityMeshes = true;
+
+            if (autoStart)
+                StartScan();
         }
 
-        meshMan.GenerateUnityMeshes = true;
-
-        if (autoStart)
-            StartScan();
-    }
-
-    // Resets UI and begins the scan.
-    public void StartScan()
-    {
-        fillBar.fillAmount = 0f;
-        scanText.text = scanningString;
-        scanCanvas.SetActive(true);
-        scanning = true;
-        OnScanStart.Invoke();
-    }
-
-    // Updates the UI based on scan progress. Ends the scan if progress bar full.
-    void Update()
-    {
-        if (!scanning)
-            return;
-        
-        scanProgress = Mathf.Min((float)meshMan.MeshRoot.transform.childCount / scanThreshold, 1f);
-        fillBar.fillAmount = Mathf.Max(scanProgress, fillBar.fillAmount);
-
-        if (scanProgress >= 1f)
+        // Resets UI and begins the scan.
+        public void StartScan()
         {
-            StartCoroutine(EndScan());
+            fillBar.fillAmount = 0f;
+            scanText.text = scanningString;
+            scanCanvas.SetActive(true);
+            scanning = true;
+            OnScanStart.Invoke();
         }
-    }
 
-    // Call if you wish to end the scan early. Set argument to true if you want to skip the complete delay.
-    public void ForceEndScan(bool immediate)
-    {
-        if (immediate)
+        // Updates the UI based on scan progress. Ends the scan if progress bar full.
+        void Update()
         {
+            if (!scanning)
+                return;
+            
+            scanProgress = Mathf.Min((float)meshMan.MeshRoot.transform.childCount / scanThreshold, 1f);
+            fillBar.fillAmount = Mathf.Max(scanProgress, fillBar.fillAmount);
+
+            if (scanProgress >= 1f)
+            {
+                StartCoroutine(EndScan());
+            }
+        }
+
+        // Call if you wish to end the scan early. Set argument to true if you want to skip the complete delay.
+        public void ForceEndScan(bool immediate)
+        {
+            if (immediate)
+            {
+                scanCanvas.SetActive(false);
+                OnScanEnd.Invoke();
+            }
+            else
+            {
+                StartCoroutine(EndScan());
+            }
+        }
+
+        // Sets UI to complete values, delays, then turns off the UI.
+        private IEnumerator EndScan()
+        {
+            fillBar.fillAmount = 1f;
+            scanText.text = completeString;
+
+            yield return new WaitForSeconds(2f);
+
             scanCanvas.SetActive(false);
             OnScanEnd.Invoke();
         }
-        else
-        {
-            StartCoroutine(EndScan());
-        }
-    }
-
-    // Sets UI to complete values, delays, then turns off the UI.
-    private IEnumerator EndScan()
-    {
-        fillBar.fillAmount = 1f;
-        scanText.text = completeString;
-
-        yield return new WaitForSeconds(2f);
-
-        scanCanvas.SetActive(false);
-        OnScanEnd.Invoke();
     }
 }
