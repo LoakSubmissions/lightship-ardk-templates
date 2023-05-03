00150b203a2288aa6b26d089815722c4ec377850
smitdylan2001 <dylan.smit01@gmail.com>
2023-05-03 11:47:48 +0200

    Minor improvements
    Done improvements:
    - Minor code performance improvements
    - Disabled mipmaps for UI sprites to reduce build size
    - Tweaked quality settings and player settings for better performance without quality loss
    - Enabled dynamic batching, improving performance for AR
    
    Things that could be done:
    Make sure UI sprites use a multiple of 2 for resolution. Even better would be to use a texture atlas for all UI sprites so they render in 1 draw call instead of 1 per sprite

--- Assets/Loak/Scripts/LoakLeaderboard.cs
+++ Assets/Loak/Scripts/LoakLeaderboard.cs
@@ -168,7 +168,7 @@ namespace Loak.Unity
 
                 item = listItems[i];
                 item.SetUIText(entries[i].Item1, entries[i].Item2.ToString());
-                item.Highlight(entries[i].Item1 == highlightedName ? true : false);
+                item.Highlight(entries[i].Item1 == highlightedName);
                 item.gameObject.SetActive(true);
             }
         }
